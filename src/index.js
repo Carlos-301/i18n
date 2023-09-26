de93147f1e42bb09e5d1ebad72694018942de031
Carlos G <ca.garciag1@uniandes.edu.co>
2023-09-26 17:21:00 -0500

    hasta retos(dynamic change of language)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
 import JobsList from "./components/jobslist";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+
+ReactDOM.render(
+    <IntlProvider locale={navigator.language || navigator.userLanguage} >
+            <JobsList/>
+    </IntlProvider>, document.getElementById("root")
+);
